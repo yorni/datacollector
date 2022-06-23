5928369271dc98384f088baa403eedeb446a0a69
yorni <yorni@inbox.ru>
2022-06-23 18:06:34 +0300

    bookticker

--- realtimeba.js
+++ realtimeba.js
@@ -86,30 +86,36 @@ function startSubscription() {
         data.A +
         " " +
         data.E;
-      console.log(resData);
-      //    () => {
-      //     const rows = [
-      //       `trades,name=test_ilp1 value=12.4 ${Date.now() * 1e6}`,
-      //       `trades,name=test_ilp2 value=11.4 ${Date.now() * 1e6}`,
-      //     ];
+      //console.log(resData);
+      client.write(resData + "\n", (err) => {
+        if (err) {
+          console.error(err);
+          process.exit(1);
+        }
+      });
+      //  () => {
+      //   const rows = [
+      //     `trades,name=test_ilp1 value=12.4 ${Date.now() * 1e6}`,
+      //     `trades,name=test_ilp2 value=11.4 ${Date.now() * 1e6}`,
+      //   ];
 
-      //     function write(idx) {
-      //       if (idx === rows.length) {
-      //         client.destroy();
-      //         return;
-      //       }
-
-      //       client.write(rows[idx] + "\n", (err) => {
-      //         if (err) {
-      //           console.error(err);
-      //           process.exit(1);
-      //         }
-      //         write(++idx);
-      //       });
+      //   function write(idx) {
+      //     if (idx === rows.length) {
+      //       client.destroy();
+      //       return;
       //     }
 
-      //     write(0);
+      //     client.write(rows[idx] + "\n", (err) => {
+      //       if (err) {
+      //         console.error(err);
+      //         process.exit(1);
+      //       }
+      //       write(++idx);
+      //     });
       //   }
+
+      //   write(0);
+      // }
     }
   });
 }
