27ef732a5b04946f1be1506cf9ff7c964a0b01dc
yorni <yorni@inbox.ru>
2022-06-25 23:01:15 +0300

    trades

--- realtimeba.js
+++ realtimeba.js
@@ -66,17 +66,59 @@ function checkBinanceWebSocketsState() {
 function startSubscription() {
   let subscribeArray = [];
 
-  subscribeArray.push(param.symbol.toLowerCase() + "@bookTicker");
+  subscribeArray.push(param.symbol.toLowerCase() + "@trade");
+  //subscribeArray.push(param.symbol.toLowerCase() + "@bookTicker");
   //}
   param.binance.futuresSubscribe(subscribeArray, (data) => {
-    if (prev_Book.E) {
-      let difAsk = (Number(prev_Book.a) - Number(data.a)) / Number(data.a);
-      let difBid = (Number(prev_Book.b) - Number(data.b)) / Number(data.b);
-      if (Math.abs(difAsk) > 0.1 || Math.abs(difBid) > 0.1) {
-        console.log(prev_Book, data, difAsk, difBid);
+    if (data.e == "bookTicker") {
+      // {
+      //   e: 'trade',
+      //   E: 1656186618377,
+      //   T: 1656186618369,
+      //   s: 'FTMUSDT',
+      //   t: 514511017,
+      //   p: '0.295400',
+      //   q: '5135',
+      //   X: 'MARKET',
+      //   m: false
+      // }
+      //CREATE TABLE 'trades' (
+      //   symb SYMBOL capacity 256 CACHE,
+      //   p DOUBLE,
+      //   q DOUBLE,
+      //   m boolean,
+      //   ts TIMESTAMP
+      // ) timestamp (ts);
+      let resData =
+        "trades," +
+        "symb=" +
+        data.s +
+        " p=" +
+        data.p +
+        ",q=" +
+        data.q +
+        ",m=" +
+        data.m +
+        " " +
+        data.E * 1e6;
+      if (!client.destroyed) {
+        client.write(resData + "\n", (err) => {
+          if (err) {
+            console.error(err);
+            process.exit(1);
+          }
+        });
       }
     }
     if (data.e == "bookTicker") {
+      if (prev_Book.E) {
+        let difAsk = (Number(prev_Book.a) - Number(data.a)) / Number(data.a);
+        let difBid = (Number(prev_Book.b) - Number(data.b)) / Number(data.b);
+        if (Math.abs(difAsk) > 0.1 || Math.abs(difBid) > 0.1) {
+          console.log(prev_Book, data, difAsk, difBid);
+        }
+      }
+
       //   {
       //     e: 'bookTicker',
       //     u: 1646689643715,
@@ -112,29 +154,6 @@ function startSubscription() {
           }
         });
       }
-      //  () => {
-      //   const rows = [
-      //     `trades,name=test_ilp1 value=12.4 ${Date.now() * 1e6}`,
-      //     `trades,name=test_ilp2 value=11.4 ${Date.now() * 1e6}`,
-      //   ];
-
-      //   function write(idx) {
-      //     if (idx === rows.length) {
-      //       client.destroy();
-      //       return;
-      //     }
-
-      //     client.write(rows[idx] + "\n", (err) => {
-      //       if (err) {
-      //         console.error(err);
-      //         process.exit(1);
-      //       }
-      //       write(++idx);
-      //     });
-      //   }
-
-      //   write(0);
-      // }
       prev_Book = Object.assign({}, data);
     }
   });
